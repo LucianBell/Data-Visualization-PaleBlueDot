b97595e55d9820848092aa49387193a23c6abb95
Lucian Bellini <lucianbellini1@gmail.com>
2023-12-20 14:01:56 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data visualization project to improve decision towards agricultural support programs in Brazil',
+    author='Lucian Bellini',
+    license='MIT',
+)
